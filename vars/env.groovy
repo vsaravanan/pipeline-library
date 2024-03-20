f3e2998672a40774faf0428fd838291162d40781
viswa <saravanan.resume@gmail.com>
2024-03-21 00:46:35 +0800

    env

---
+++ vars/env.groovy
@@ -0,0 +1,13 @@
+def call() {
+
+      def jenkinsConfig = env.jenkins_config_home
+
+      jenkinsConfig = "${jenkinsConfig}/${JOB_NAME}"
+      echo "JENKINS_CONFIG: ${jenkinsConfig}"
+
+      echo "JOB_NAME : ${JOB_NAME}"                // tutonui
+      echo "BUILD_URL : ${BUILD_URL}"              // https://web.saravanjs.com/job/tutonui/60/
+      echo "JENKINS_HOME : ${JENKINS_HOME}"        // /var/lib/jenkins
+      echo "WORKSPACE : ${WORKSPACE}"              // /var/lib/jenkins/workspace/tutonui
+
+}
