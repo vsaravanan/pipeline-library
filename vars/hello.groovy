b9a26dd3f84e8af80500cddb8fada1f43c43516b
viswa <saravanan.resume@gmail.com>
2024-03-21 00:09:29 +0800

    call

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -2,6 +2,6 @@ def call(name) {
     sh "echo ${name} hello world"
 }
 
-def addNumbers(int num1, int num2) {
-    return num1 + num2
-}
+// def addNumbers(int num1, int num2) {
+//     return num1 + num2
+// }
