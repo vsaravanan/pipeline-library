1d04f5dd8345ee80e4a29b24947b0bbbbbb6dc12
viswa <saravanan.resume@gmail.com>
2024-03-21 00:25:37 +0800

    appVer

--- vars/calc.groovy
+++ vars/calc.groovy
@@ -1,5 +1,6 @@
 def add(x,y) {
     echo " sum of $x and $y is ${x + y}"
-    echo " JOB_NAME mvs ${JOB_NAME} "
+    echo " JOB_NAME  ${JOB_NAME}   "
+    echo " appVer  ${appVer}   "
     return x + y
 }
