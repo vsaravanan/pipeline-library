145d602c183103d3aac9f639c682178fbb787663
viswa <saravanan.resume@gmail.com>
2024-03-21 23:45:50 +0800

    error in emailext

--- vars/email.groovy
+++ vars/email.groovy
@@ -13,15 +13,15 @@ def failed( appVer, lastCommitMessage) {
 
 def sendmsg( appVer, lastCommitMessage, status) {
 
-      echo "jenkins job ${status} ${appVer}"
-      body = "jenkins job ${status} \n " +
-                " job name : ${JOB_NAME} \n " +
-                " Version : ${appVer} \n " +
-                " Jenkins : ${BUILD_URL} \n " +
-                " Commit Message : ${lastCommitMessage} "
-      emailext body: body,
-                subject: "jenkins job ${appVer} was deployed ${status}",
-                to: 'saravanan.resume@gmail.com',
-                from: 'jenkins'
+      // echo "jenkins job ${status} ${appVer}"
+      // body = "jenkins job ${status} \n " +
+      //           " job name : ${JOB_NAME} \n " +
+      //           " Version : ${appVer} \n " +
+      //           " Jenkins : ${BUILD_URL} \n " +
+      //           " Commit Message : ${lastCommitMessage} "
+      // emailext body: body,
+      //           subject: "jenkins job ${appVer} was deployed ${status}",
+      //           to: 'saravanan.resume@gmail.com',
+      //           from: 'jenkins'
 
 }
