2475ccf535ef94f30dc879b45fe5db381fc57dd0
viswa <saravanan.resume@gmail.com>
2024-03-21 22:55:28 +0800

    clean

---
+++ vars/clean.groovy
@@ -0,0 +1,4 @@
+def call() {
+    sh "pwd"
+    sh "rm -rf *"
+}
--- vars/envi.groovy
+++ vars/envi.groovy
@@ -5,6 +5,7 @@ def call() {
       jenkinsConfig = "${jenkinsConfig}/${JOB_NAME}"
       echo "JENKINS_CONFIG: ${jenkinsConfig}"
 
+      echo "JAVA_HOME: ${JAVA_HOME}"
       echo "JOB_NAME : ${JOB_NAME}"                // tutonui
       echo "BUILD_URL : ${BUILD_URL}"              // https://web.saravanjs.com/job/tutonui/60/
       echo "JENKINS_HOME : ${JENKINS_HOME}"        // /var/lib/jenkins
